f48583cd4fff8857346d34a44a2cc8ba2aa562b7
Dinesh Prajapati <dinesh4manit@gmail.com>
2022-06-02 01:42:03 +0530

    Initial commit

---
+++ app/src/main/java/com/dinesh/toastapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dinesh.toastapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dinesh.toasty.Toaster;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toaster.showToast(this, "helloDP");
+    }
+}
